ad70e9a28f15be9f11e1a7a86b94098368be5700
goran <goranaviani@gmail.com>
2018-06-17 18:30:54 +0200

    In game_functionality.py made 2 new functions. One to store keyup and other for keydown.

--- game_functions.py
+++ game_functions.py
@@ -2,28 +2,35 @@ import sys
 import pygame
 
 def check_events(space_ship):
+
+
+    # When Pygame detects KEYDOWN event I check if right key is pressed. If so increse position for 1 pixel
+    # KEYDOWN == when key is pressed down, KEYUP == when key is released
+    def check_keydown_events(event, space_ship):
+        # IF right key is pressed ship is moving right
+        if event.key == pygame.K_RIGHT:
+            space_ship.moving_right = True
+        # elseIF left key is pressed ship is moving right
+        elif event.key == pygame.K_LEFT:
+            space_ship.moving_left = True
+
+    def check_keyup_events(event, space_ship):
+        # IF the right key is released ship has stopped moving right
+        if event.key == pygame.K_RIGHT:
+            space_ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            space_ship.moving_left = False
+
     # Listen for keyboard and mouse envents
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-        #When Pygame detects KEYDOWN event I check if right key is pressed. If so increse position for 1 pixel
-        #KEYDOWN == when key is pressed down, KEYUP == when key is released
         elif event.type == pygame.KEYDOWN:
-            # IF right key is pressed ship is moving right
-            if event.key == pygame.K_RIGHT:
-                space_ship.moving_right = True
-            # elseIF left key is pressed ship is moving right
-            elif event.key == pygame.K_LEFT:
-                space_ship.moving_left = True
-
+            check_keydown_events(event, space_ship)
 
         elif event.type == pygame.KEYUP:
-            # IF the right key is released ship has stopped moving right
-            if event.key == pygame.K_RIGHT:
-                space_ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                space_ship.moving_left = False
+            check_keyup_events(event, space_ship)
 
 
 
